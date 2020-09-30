8a6138805f5b8b3536db1b9129b9e986c8ade164
JessicaQueiroz <jehqueiroz01@gmail.com>
2020-09-30 19:47:23 -0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	        String nome = "Tipos de Varíáveis";
+	        int numero = 50;
+	        double pi = 3.14;
+	        boolean bool = true;
+
+	        System.out.printf("%s - String, Int - %d, Double - %.2f, Boolean - %s", nome, numero, pi, bool);
+	    }
+}
